549e8114b5a9a893d63b11578840269af5d6cbda
jimmyahalpara <jimmyahalpara123@gmail.com>
2024-12-30 02:10:41 +0530

    Laravel Translateable

--- demo-app-1/app/Models/Post.php
+++ demo-app-1/app/Models/Post.php
@@ -4,13 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+    use HasTranslations;
 
     protected $fillable = ['title', 'content', 'user_id'];
+    public $translatable = ['summary'];
 
     public function user()
     {
